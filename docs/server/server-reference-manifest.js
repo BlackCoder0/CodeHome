64e3f22d1a2de12f6ca974906df6521b61b33530
Code-Liang <2943984952@qq.com>
2025-06-25 15:43:43 +0800

    刚刚的打包有失误

--- docs/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d5Fme5jOHz1dPTguZqW5KK3OMoWkZY/8LTpLrYK9Zxs=\"\n}"
--- docs/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/node_modules_next_dist_1a6ee436._.js",
-  "static/chunks/src_components_BaiduAnalytics_tsx_b5090627._.js"
-],
-    source: "dynamic"
-});
